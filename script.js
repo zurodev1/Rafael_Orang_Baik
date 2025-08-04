48445bd820b423210e1b7c9cb430323c4dd1cc5f
zurodev1 <zurrdeveloper@gmail.com>
2025-08-04 12:22:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const typed = new Typed('#typed', {
   strings: ["Jangan Pernah Takut Mati","Karna Di Dalam Kehidupan Ini","Kita Ibarat Hanya","Mampir Untuk Minum","Dan Menyiapkan Bekal","Untuk Pulang Dan Menghadap","Kepadanya Yang Kekal."],
     typeSpeed: 100,
     backSpeed: 50,
-    backDelay: 3000,
+    backDelay: 100,
     startDelay: 30,
     loop: true
 });
