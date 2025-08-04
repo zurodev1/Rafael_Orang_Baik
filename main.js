ec263ac005f703270cf65e94c612907a0d010dea
zurodev1 <zurrdeveloper@gmail.com>
2025-08-04 11:57:15 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["RASA SAKIT ITU","KETIKA KAMU MENGABAIKAN","PERKATAAN ORANG TUAMU","DAN AKHIRNYA KAU SADAR","BETAPA PENTINGNYA","SEMUA OCEHAN ITU"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    backDelay: 50,
+    loop: true
+});
---
+++ script.js
@@ -0,0 +1,8 @@
+const typed = new Typed('#typed', {
+  strings: ["Jangan Pernah Takut Mati","Karna Di Dalam Kehidupan Ini","Kita Ibarat Hanya","Mampir Untuk Minum","Dan Menyiapkan Bekal","Untuk Pulang Dan Menghadap","Kepadanya Yang Kekal."],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 3000,
+    startDelay: 30,
+    loop: true
+});
